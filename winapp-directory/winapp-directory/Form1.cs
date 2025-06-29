d39ae7ff84cf8001a751a51e681ec6a6ccc66abf
sumeta.pon <sumeta.pon@gmail.com>
2025-06-29 00:29:38 +0000

    winapp-directory

---
+++ winapp-directory/winapp-directory/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winapp_directory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(@"C:\Temp"))
+            {
+                MessageBox.Show("Directory Exists!", "OK");
+            }
+            else
+            {
+                MessageBox.Show("Directory not Exists!", "Error");
+            }
+        }
+    }
+}
